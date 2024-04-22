1d43339869137adf6fcf82e8036d873169e0030c
terminicus <terminicus@users.noreply.github.com>
2024-04-22 19:22:23 +0000

    from_into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,37 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default()
+        }
+
+        let split : Vec<&str>= s.split(',').collect();
+        
+        if(split.len() < 2) {
+            return Person::default()
+        }
+
+        let name = split[0];
+
+        if name.is_empty(){
+            return Person::default();
+        }
+
+
+        let age = split[1];
+        let age = age.parse::<usize>();
+
+        if age.is_err(){
+            return Person::default();
+        }
+
+        return Person {
+            name: name.to_string(),
+            age: age.unwrap()
+        }
     }
 }
 
